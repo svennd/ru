b031262da7158e9537859741acd564ebdc5910b5
Alex Payne <alexander.payne@nottingham.ac.uk>
2020-02-12 16:22:49 +0000

    Add comments, refactor needs to create a function (utils.py) that can be used for explicit validation and passive validation when running ReadUntil

--- ru/validate.py
+++ ru/validate.py
@@ -19,13 +19,17 @@ def main():
     parser.add_argument("-t", help="Validate target file", action="store_true")
     args = parser.parse_args()
 
+    # Open TOML file as dictionary
     d = toml.load(args.toml)
+    # Open schema as dictionary
     with open(args.schema) as fh:
         s = json.load(fh)
 
+    # Attempt to validate the TOML (d) against the schema (s)
     try:
         jsonschema.validate(d, s)
     except jsonschema.exceptions.ValidationError as err:
+        # Validation failed, emit error message and quit
         print(
             "😾 this TOML file is not valid and may not work with Read Until:",
             err,
@@ -33,6 +37,10 @@ def main():
         )
         sys.exit(1)
 
+    # Validation passed, we have a couple of edge cases that need to be handled
+    #  - targets can be in a txt file on disk, these need to be read and checked
+
+    # Get regex patterns from json schema
     patterns = [
         v
         for p in s["definitions"]["conditions"]["patternProperties"]["^[0-9]+$"][
@@ -41,7 +49,10 @@ def main():
         for k, v in p.items()
     ]
 
+    # If validating an external file:
     if args.t:
+        # For each condition, get targets from in a file, and run against the
+        #  patterns from the schema
         for k, v in d["conditions"].items():
             if not isinstance(v, dict):
                 continue
